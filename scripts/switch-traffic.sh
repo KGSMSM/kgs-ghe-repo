7346824e96f442870e32889fbe024fbf1af47260
nealbaker-msm <neal.baker@moneysupermarket.com>
2015-08-26 13:58:38 +0100

    Updated script preferred primary

--- scripts/switch-traffic.sh
+++ scripts/switch-traffic.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#switcher to route traffic through forwarder 1 or 2
-#switcher to route traffic through forwarder 1 or 2
+#switcher to route traffic through forwarder 1 or 2 xxxxx
 sudo /sbin/iptables-restore < /etc/iptables.$1
 
 exit
