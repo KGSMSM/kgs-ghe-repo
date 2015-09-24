1f4019f07c75d7e9465768edbdc5854f386b5b0c
nealbaker-msm <neal.baker@moneysupermarket.com>
2015-09-24 15:23:45 +0100

    Uploaded latest code base

--- scripts/master.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-EIP=52.18.153.196
-INSTANCE_ID=i-354bb599
-
-/usr/local/bin/aws ec2 disassociate-address --public-ip $EIP
-/usr/local/bin/aws ec2 associate-address --public-ip $EIP --instance-id $INSTANCE_ID
-
